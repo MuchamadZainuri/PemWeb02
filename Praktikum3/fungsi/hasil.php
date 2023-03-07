e56e5fea6b83a7d202bfd1f54b0c697c4fed2db0
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2023-03-07 14:29:28 +0700

    Tugas & Praktikum ke 3

---
+++ Praktikum3/fungsi/hasil.php
@@ -0,0 +1,69 @@
+<?php
+
+// Jika Belum mengisi form maka tidak dapat pergi ke halaman hasil
+if (!isset($_POST['submit'])) {
+    header("location: index.php");
+    exit;
+}
+
+
+// menyimpan inputan user kedalam variabel
+$nama = $_POST['nama'];
+$matkul = $_POST['matkul'];
+$uts = $_POST['uts'];
+$uas = $_POST['uas'];
+$tugas = $_POST['tugas'];
+
+//menentukan nilai akhir
+$nilai_akhir = ($uts + $uas + $tugas) / 3;
+
+// Mengambil sekaligus menjalankan skrip yang ada di library fungsi untuk cetak kelulusan dan grade
+require_once 'libfungsi.php';
+$_nilai = $nilai_akhir;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Mata kuliah</th>
+                <th scope="col">UTS</th>
+                <th scope="col">UAS</th>
+                <th scope="col">TUGAS</th>
+                <th scope="col">NILAI AKHIR</th>
+                <th scope="col">GRADE</th>
+                <th scope="col">KETERANGAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td> <?= $nama ?> </td>
+                <td> <?= $matkul ?> </td>
+                <td> <?= $uts ?> </td>
+                <td> <?= $uas ?> </td>
+                <td> <?= $tugas ?> </td>
+                <td> <?= $nilai_akhir ?> </td>
+                <td> <?= grade($_nilai) ?> </td>
+                <td> <?= kelulusan($_nilai) ?> </td>
+            </tr>
+            </tbody>
+    </table>
+</body>
+
+</html>
--- Praktikum3/fungsi/index.php
+++ Praktikum3/fungsi/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas2-Pertemuan2</title>
+    <title>Praktikum 3</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
