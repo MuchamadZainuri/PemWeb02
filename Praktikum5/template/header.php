4c2eb80164b01c923163e8af8165852e58fdfda2
Muchamad Zainuri <muchamadzainuri6@gmail.com>
2023-03-27 15:07:15 +0700

    Tugas Pertemuan Ke 5 terakhir

--- Praktikum5/template/header.php
+++ Praktikum5/template/header.php
@@ -25,7 +25,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index.php.php" class="nav-link">Tabel Produk</a>
+                    <a href="index.php" class="nav-link">Tabel Produk</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
                     <a href="list_pelanggan.php" class="nav-link">Table Pelanggan</a>
